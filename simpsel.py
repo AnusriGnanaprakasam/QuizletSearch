acbed735b7042acc00f8708d2a6ab9638655d277
AnusriGnanaprakasam <nunu@arch.localdomain>
2022-09-25 17:48:07 -0700

    prints out text in decks

--- simpsel.py
+++ simpsel.py
@@ -9,7 +9,17 @@ import time
 
 driver = webdriver.Firefox(executable_path=r"/home/nunu/Tools/geckodriver")
 
-def quizletdecks():
-    driver.get("https://quizlet.com/search?query=AP-Calc&type=sets")
+def quizletdecks(query):
+    driver.get(f"https://quizlet.com/search?query={query}&type=sets")
     #format: change url for whatever topic you want to search for
-quizletdecks()
+    #bodyclass=  driver.find_element(By.XPATH,value="/html/body/div[4]/main/div")
+    site = driver.find_element(By.CLASS_NAME,value="site")
+    main = site.find_element(By.ID,value="page") 
+    searchResultsPage = main.find_element(By.CLASS_NAME,value="SearchResultsPage") 
+    setview = searchResultsPage.find_element(By.CLASS_NAME,value="SetsView-resultList") 
+    Decks = setview.find_elements(By.CLASS_NAME,value="SearchResultsPage-result")
+    print(type(Decks))
+    for deck in range(len(Decks)):
+        print(Decks[deck].text)
+quizletdecks('AP-Calc')# query should be given with - where the spaces should be
+

ea9adbf3032ee4dc9a747a3986d24f8e6d2d1fc0
AnusriGnanaprakasam <nunu@arch.localdomain>
2022-11-15 22:15:42 -0700

    filtered for terms

--- simpsel.py
+++ simpsel.py
@@ -1,35 +1,53 @@
 #Selenium tut
 
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-import copy
-driver = webdriver.Firefox(executable_path=r"/home/nunu/Tools/geckodriver")
+import re
 
+service = Service(r"/home/nunu/Tools/geckodriver" )
+driver = webdriver.Firefox(service=service)
 def quizletdecks(query):
 
     driver.get(f"https://quizlet.com/search?query={query}&type=sets")
+
     #format: change url for whatever topic you want to search for
     # work but i don't need it bodyclass=  driver.find_element(By.XPATH,value="/html/body/div[4]/main/div")
-    site = driver.find_element(By.CLASS_NAME,value="site")
-    main = site.find_element(By.ID,value="page") 
-    searchResultsPage = main.find_element(By.CLASS_NAME,value="SearchResultsPage") 
-    setview = searchResultsPage.find_element(By.CLASS_NAME,value="SetsView-resultList") 
+    #site = driver.find_element(By.CLASS_NAME,value="site")
+    #main = driver.find_element(By.ID,value="page") 
+    #searchResultsPage = driver.find_element(By.CLASS_NAME,value="SearchResultsPage") 
+    #i did try to go layer by layer to make sure it could find elements
+
+    setview = driver.find_element(By.CLASS_NAME,value="SetsView-resultList") 
     Decks = setview.find_elements(By.CLASS_NAME,value="SearchResultsPage-result")
     print(type(Decks))
     previewsel = []
     for deck in range(len(Decks)):
         previewsel.append(Decks[deck].text)
     print(previewsel)
-    #make a dictionary and populate with element index and then the nums found in the element(if there is a 5 with another
-    #nun then that means that it is five starts and should defn be picked regardless of num of terms
-    for element_text in previewsel:
-        for char in element_text:
-            if char.isdigit(): 
-                pass    
-# Lesson 6.5: SET OPTI 
-quizletdecks('AP-Calc')# query should be given with - where the spaces should be
 
+    
+    #filter by num of terms and see if there are stars:
+    for element_text in previewsel:
+        termraw= re.search(r'(\d{2} terms)',element_text) #need to figure out how to make it so that the thing in braces and be whatever num
+        print(termraw)
+       # termnum = termraw[0:2] #how to get search to only return the string it find(it sould be in geeks for geeks article)
+        stars = re.search(r"(\n\d{1} ()",element_text)
+        print(stars)
+       # print(termnum+" "+stars)
+        
+quizletdecks('AP-Calc')# query should be given with - where the spaces should be
+                                                                                              
+'''first filter for highest amount of terms
+[nunu@arch WebScraping]$ echo then filter for categorize 5 starts and lower
+then filter for categorize 5 starts and lower
+[nunu@arch WebScraping]$ echo then categorize for num of views
+then categorize for num of views
+[nunu@arch WebScraping]$ echo then check for name
+then check for name
+[nunu@arch WebScraping]$ echo and do the find element by txt thing
+and do the find element by txt thing
+'''
